2933477b1edb758d0e52fa987fd78028c5b4350d
Adam Popławski <adampoplawski2008@gmail.com>
2020-05-12 12:50:02 +0200

    BEM-ologia

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 const fisrtName='Adam';
 const age=43
 console.log(`Witajcie, nazywam się ${fisrtName} i mam ${age} lata.`);
+
+const paragraph = document.querySelector('.about__description--js');
+paragraph.innerHTML='coś';
