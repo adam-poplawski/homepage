090bed082dd0823002cbf866ab9da958f3e24b86
Adam Popławski <adampoplawski2008@gmail.com>
2020-06-07 18:08:34 +0200

    grid added

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-const fisrtName='Adam';
+const firstName='Adam';
 const age=43
-console.log(`Witajcie, nazywam się ${fisrtName} i mam ${age} lata.`);
+console.log(`Witajcie, nazywam się ${firstName} i mam ${age} lata.`);
 
 const paragraph = document.querySelector('.about__description--js');
 paragraph.innerHTML='coś';
+
+const heading = document.querySelector('.main_heading--js');
+heading.innerHTML=`Witajcie, nazywam się ${firstName} i mam ${age} lata.`;
+
+function hello (secondName, age) {
+    console.log(`Witajcie, nazywam się ${secondName} i mam ${age} lata.`);
+}
+
+const secondName='Marcin';
+hello (secondName, age);
